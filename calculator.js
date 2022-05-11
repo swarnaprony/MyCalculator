bc388207d3a0c61eedfc9a17aa4f89de4b2fb283
Swarna Sarker <swarnaprony81@gmail.com>
2022-05-11 18:32:40 +0200

    first commit

---
+++ calculator.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+app.post("/", function(req, res){
+    console.log(req.body);
+    var num1 = Number(req.body.num1);
+    var num2= Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    res.send("The result of the calculation is: " + result);
+});
+
+
+app.get('/bmicalculator', function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (height* height);
+
+    res.send("The result of the bmi calculation is: " + bmi);
+});
+
+app.listen(4000, function(){
+    console.log("Server Started on Port 4000.");
+});
